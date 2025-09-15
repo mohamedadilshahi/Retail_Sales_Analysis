427ece58cb927c02a1bef9bbd795193e3742b7de
Mohamed Adilshahi <mdadilshanadaf@gmail.com>
2025-09-15 20:07:15 +0530

    Update Retail Sales Analaysis Project.sql

--- Retail Sales Analaysis Project.sql
+++ Retail Sales Analaysis Project.sql
@@ -5,6 +5,20 @@
 -- exploring datasets, and performing exploratory data analysis (EDA) to uncover patterns and insights. I created and managed a retail sales database,
 -- wrote SQL queries to answer key business questions, and analyzed sales performance across products, regions, and time periods.
 
+## Objectives
+-- 1. Set up a retail sales database: Create and populate a retail sales database with the provided sales data.
+-- 2. Data Cleaning: Identify and remove any records with missing or null values.
+-- 3. Exploratory Data Analysis (EDA): Perform basic exploratory data analysis to understand the dataset.
+-- 4. Business Analysis: Use SQL to answer specific business questions and derive insights from the sales data.
+
+## Project Structure
+
+## 1. Database Setup
+-- Database Creation: Designed and created a database named 'Retail_sales' to manage retail sales data.
+/* Table Creation: Built a table retail_sales with structured columns including transaction ID, sale date, sale time, customer details 
+(ID, gender, age), product category, quantity sold, price per unit, cost of goods sold (COGS), and total sale amount. This setup ensured organized
+storage of data for analysis and querying.
+*/
 	
 
 create database Retail_sales
@@ -198,6 +212,7 @@ order by total_customers desc
 select  category, COUNT(distinct customer_id) as 'Unique_c_id' from R_sales
 group by category
 order by Unique_c_id desc
+
 
 
 
